b19e010f7aa9caba863f275068c5ff8484855d83
Julia <145168563+julia-aph@users.noreply.github.com>
2024-02-29 01:37:34 -0600

    input system!! game loop!!
    woah!!

--- hi.c
+++ hi.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
-#include <iso646.h>
 #include <time.h>
+#include <pthread.h>
+#include <windows.h>
 
 struct String
 {
@@ -169,8 +169,8 @@ struct BlockMap
     unsigned int height;
     unsigned int area;
 
-    unsigned int x;
-    unsigned int y;
+    int x;
+    int y;
 
     unsigned char rotation;
 
@@ -273,12 +273,13 @@ struct String *DrawBufferToString(struct Buffer *buffer)
     int stringSize = buffer->area + buffer->height - 1;
     struct String *string = NewStringAllocate(stringSize);
 
+    ConcatChars(string, "\x1b[1;1H\x1b[2J", 10);
     Concat(string, GenerateColorCode( &(unsigned char) {0}, 1 ));
 
     unsigned char lastColor = 0;
     for(int i = 0; i < buffer->area; i++)
     {
-        if(i % buffer->width == 0 and i != 0 and i != buffer->area - 1)
+        if(i % buffer->width == 0 && i != 0 && i != buffer->area - 1)
         {
             ConcatChar(string, '\n');
         }
@@ -297,6 +298,22 @@ struct String *DrawBufferToString(struct Buffer *buffer)
     }
 
     Concat(string, GenerateColorCode( &(unsigned char) {0}, 1 ));
+
+    static int frame = 0;
+    static double lastFrameTime = 0;
+
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    double now = (double)ts.tv_sec + ((double)ts.tv_nsec / 1000000000.0);
+
+    char fun[20] = { 0 };
+
+    int bitch = sprintf(fun, " %d %f", frame, now - lastFrameTime);
+
+    ConcatChars(string, fun, bitch);
+
+    frame++;
+    lastFrameTime = now;
 
     return string;
 }
@@ -383,6 +400,28 @@ void OverlayBlockMap(struct BlockMap *dest, struct BlockMap *source)
     }
 }
 
+struct FumotrisGameState
+{
+    struct BlockMap board;
+    struct BlockMap current;
+};
+
+unsigned int Hash(void *item, int size)
+{
+    unsigned char* data = (unsigned char*)item;
+
+    unsigned int h = 98317;
+
+    for(int i = 0; i < size; i++)
+    {
+        h ^= data[i];
+        h *= 0x5bd1e995;
+        h ^= h >> 15;
+    }
+
+    return h;
+}
+
 struct DictionaryBucket
 {
     void *key;
@@ -395,80 +434,80 @@ struct Dictionary
     struct DictionaryBucket *buckets;
     unsigned int capacity;
     unsigned int used;
+
+    unsigned int keySize;
 };
 
-struct Dictionary *NewDictionaryAllocate(unsigned int capacity)
+struct Dictionary *NewDictionaryAllocate(unsigned int capacity, unsigned int keySize)
 {
     struct Dictionary *dict = malloc(sizeof(struct Dictionary));
     dict->buckets = calloc(capacity, sizeof(struct DictionaryBucket));
     dict->capacity = capacity;
     dict->used = 0;
+
+    dict->keySize = keySize;
 }
 
-struct Dictionary *NewDictionary()
+struct Dictionary *NewDictionary(unsigned int keySize)
 {
-    return NewDictionaryAllocate(16);
+    return NewDictionaryAllocate(16, keySize);
 }
 
-void DictAdd(struct Dictionary *dict, void *key, unsigned int hash, void *value)
+int dictLinearProbe(struct Dictionary *dict, int index, int search)
 {
+    if(dict->buckets[index].hash == search)
+        return index;
+
+    for(int i = 1; i < dict->capacity; i++)
+    {
+        unsigned int check = (index + i / 2 - (i % 2) * i) % dict->capacity;
+        if(dict->buckets[check].hash == search)
+            return check;
+    }
+
+    return -1;
+}
+
+void DictAdd(struct Dictionary *dict, void *key, void *value)
+{
+    unsigned int hash = Hash(key, dict->keySize);
     unsigned int index = hash % dict->capacity;
 
-    if(dict->buckets[index].hash != 0)
-    {
-        printf("collision: %c (%d)@i%d\n", *(char*)dict->buckets[index].key, dict->buckets[index].hash, index);
-        printf("attempt:   %c (%d)\n", *(char*)key, hash);
-    }
-    else
-    {
-        dict->buckets[index].key = key;
-        dict->buckets[index].hash = hash;
-        dict->buckets[index].value = value;
-        dict->used++;
-    }
+    int probe = dictLinearProbe(dict, index, 0);
+    if(probe == -1)
+        return; // Dictionary is full
+
+    dict->buckets[probe] = (struct DictionaryBucket){ key, hash, value };
+    dict->used++;
 }
 
-void DictRemove(struct Dictionary *dict, unsigned int hash)
+/*void DictRemove(struct Dictionary *dict, void *key) // Make compatible with linear probe
 {
+    unsigned int hash = Hash(key, dict->keySize);
     unsigned int index = hash % dict->capacity;
     dict->buckets[index].value = 0;
-}
+}*/
 
-void *DictGet(struct Dictionary *dict, unsigned int hash)
+void *DictGet(struct Dictionary *dict, void *key)
 {
+    unsigned int hash = Hash(key, dict->keySize);
     unsigned int index = hash % dict->capacity;
-    return dict->buckets[index].value;
+
+    int probe = dictLinearProbe(dict, index, hash);
+    if(probe == -1)
+        return 0; // Not found
+
+    return dict->buckets[probe].value;
 }
 
-unsigned char DictContainsKey(struct Dictionary *dict, unsigned int hash)
+unsigned char DictContainsKey(struct Dictionary *dict, void *key)
 {
+    unsigned int hash = Hash(key, dict->keySize);
     unsigned int index = hash % dict->capacity;
-    return dict->buckets[index].value != 0;
+
+    int probe = dictLinearProbe(dict, index, hash);
+    return probe == -1;
 }
-
-unsigned int Hash(void *item, int size)
-{
-    char *p = (char*)item;
-
-    unsigned int hash = 0;
-    for(int i = 0; i < size; i++)
-    {
-        hash += p[i] * 31;
-    }
-
-    return hash;
-}
-
-unsigned int HashInt(int item) { return Hash(&item, sizeof(int)); }
-unsigned int HashChar(char item) { return Hash(&item, sizeof(char)); }
-
-enum Controls
-{
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
-};
 
 struct InputAxis
 {
@@ -479,33 +518,47 @@ struct InputAxis
 struct Controller
 {
     struct Dictionary *keybinds; // char: int
-    struct Dictionary *axes; // int: inputAxis
-
+    struct Dictionary *axes; // int: inputAxis*
     struct InputAxis *inputAxes;
+
+    pthread_mutex_t mutex;
 };
 
 struct Controller *NewController(char *keys, int *codes, int count)
 {
     struct Controller *controller = malloc(sizeof(struct Controller));
 
-    struct Dictionary *keybinds = NewDictionaryAllocate(count);
-    struct Dictionary *axes = NewDictionaryAllocate(count);
+    struct Dictionary *keybinds = NewDictionaryAllocate(count, sizeof(unsigned char));
+    struct Dictionary *axes = NewDictionaryAllocate(count, sizeof(int));
     struct InputAxis *inputAxes = calloc(count, sizeof(struct InputAxis));
     for(int i = 0; i < count; i++)
     {
-        DictAdd(keybinds, keys + i, HashChar(keys[i]), codes + i);
-        DictAdd(axes, codes + i, HashInt(codes[i]), inputAxes + i);
+        DictAdd(keybinds, keys + i, codes + i);
+        DictAdd(axes, codes + i, &inputAxes[i]);
     }
 
     controller->keybinds = keybinds;
     controller->axes = axes;
     controller->inputAxes = inputAxes;
 
+    controller->mutex = PTHREAD_MUTEX_INITIALIZER;
+
     return controller;
 }
 
-HANDLE standardInputHandle;
-INPUT_RECORD inputBuffer[32];
+struct InputAxis *ControllerGetAxis(struct Controller *controller, char key)
+{
+    int *code = DictGet(controller->keybinds, &key);
+    if(code == 0)
+        return 0;
+
+    return DictGet(controller->axes, code);
+}
+
+struct InputAxis *ControllerAxis(struct Controller *controller, int code)
+{
+    return DictGet(controller->axes, &code);
+}
 
 double GetTime()
 {
@@ -515,80 +568,149 @@ double GetTime()
     return (double)ts.tv_sec + ((double)ts.tv_nsec / 1000000000.0);
 }
 
-void WindowsInputInit()
+enum Controls
 {
-    standardInputHandle = GetStdHandle(STD_INPUT_HANDLE);
-    SetConsoleMode(standardInputHandle, ENABLE_WINDOW_INPUT);
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
+    ROTATE_CCW,
+    ROTATE_CW,
+    ESC
+};
+
+/*
+enum Controls
+{
+    LEFT,
+    RIGHT,
+    SOFT_DROP,
+    HARD_DROP,
+    ROTATE_CCW,
+    ROTATE_CW,
+    ROTATE_180,
+    SWAP,
+    ESC
+}
+*/
+
+struct Windows
+{
+    HANDLE standardInputHandle;
+    HANDLE timer;
+    INPUT_RECORD inputBuffer[32];
+};
+
+void WindowsInit(struct Windows *win)
+{
+    win->standardInputHandle = GetStdHandle(STD_INPUT_HANDLE);
+    if(win->standardInputHandle == INVALID_HANDLE_VALUE)
+        exit(1);
+
+    win->timer = CreateWaitableTimer(NULL, TRUE, NULL);
+    if(!win->timer)
+        exit(1);
+
+    BOOL consoleModeOk = SetConsoleMode(win->standardInputHandle, ENABLE_WINDOW_INPUT);
+    if(!consoleModeOk)
+        exit(1);
 }
 
-void WindowsKeyEvent(KEY_EVENT_RECORD keyEvent)
+void WindowsKeyEvent(KEY_EVENT_RECORD keyEvent, double timestamp, struct Controller *controller)
 {
+    char key;
+    switch(keyEvent.wVirtualKeyCode)
+    {
+        case VK_ESCAPE: key = 27; break;
+        default: key = keyEvent.uChar.AsciiChar;
+    }
     
+    struct InputAxis *axis = ControllerGetAxis(controller, key);
+    if(axis == 0) return;
+
+    axis->isPressed = keyEvent.bKeyDown;
+    if(keyEvent.bKeyDown)
+    {
+        axis->timeLastPressed = timestamp;
+    }
 }
 
-void WindowsInput()
+void WindowsResizeEvent(WINDOW_BUFFER_SIZE_RECORD windowEvent, struct Controller *controller)
+{
+
+}
+
+void BlockWindowsInput(struct Windows *win, struct Controller *controller)
 {
     DWORD recordCount;
-    ReadConsoleInput(
-        standardInputHandle,    // input buffer handle
-        inputBuffer,            // buffer to read into
-        32,                     // size of read buffer
-        &recordCount            // number of records read
-    );
+    BOOL readOk = ReadConsoleInput(win->standardInputHandle, win->inputBuffer, 32, &recordCount);
+    if(!readOk)
+        exit(1);
 
     double now = GetTime();
 
+    pthread_mutex_lock(&controller->mutex);
+
     for (int i = 0; i < recordCount; i++)
     {
-        if(inputBuffer[i].EventType == KEY_EVENT)
+        if(win->inputBuffer[i].EventType == KEY_EVENT)
         {
-            WindowsKeyEvent(inputBuffer[i].Event.KeyEvent);
+            WindowsKeyEvent(win->inputBuffer[i].Event.KeyEvent, now, controller);
         }
-        else if(inputBuffer[i].EventType == WINDOW_BUFFER_SIZE_EVENT)
+        /*else if(win->inputBuffer[i].EventType == WINDOW_BUFFER_SIZE_EVENT)
         {
-
-        }
+            WindowsResizeEvent(win->inputBuffer[i].Event.WindowBufferSizeEvent, controller);
+        }*/
     }
+
+    pthread_mutex_unlock(&controller->mutex);
+}
+
+void WindowsWait(struct Windows *win, double seconds)
+{
+    LARGE_INTEGER duration;
+    duration.QuadPart = (long)(-10000000.0 * seconds);
+
+    SetWaitableTimer(win->timer, &duration, 0, NULL, NULL, FALSE);
+    WaitForSingleObject(win->timer, INFINITE);
+}
+
+struct InputThreadArgs
+{
+    struct Windows *win;
+    struct Controller *controller;
+};
+
+void *InputThread(void *inputThreadArgs)
+{
+    struct InputThreadArgs *args = inputThreadArgs;
+
+    while(ControllerAxis(args->controller, ESC)->timeLastPressed == 0)
+    {
+        BlockWindowsInput(args->win, args->controller);
+    }
+}
+
+void Update()
+{
+
 }
 
 int main()
 {
-    char defaultKeys[4] = { 'w', 'a', 's', 'd' };
-    int codes[4] = { UP, LEFT, DOWN, RIGHT };
-    struct Controller *controller = NewController(defaultKeys, codes, 4);
+    char defaultKeys[7] = { 'a', 'd', 'w', 's', 'q', 'e', 27 };
+    int codes[7] = { LEFT, RIGHT, UP, DOWN, ROTATE_CCW, ROTATE_CW, ESC };
 
-    struct InputAxis *axis = DictGet(controller->axes, HashInt(DOWN));
+    struct Controller *controller = NewController(defaultKeys, codes, 7);
 
-    for(int i = 0; i < controller->keybinds->capacity; i++)
-    {
-        char *k = (char*)controller->keybinds->buckets[i].key;
-        int *v = (int*)controller->keybinds->buckets[i].value;
-        int h = controller->keybinds->buckets[i].hash;
+    struct Windows win = { 0 };
+    WindowsInit(&win);
 
-        char ko;
-        if(k == 0)
-            ko = '0';
-        else
-            ko = *k;
 
-        int vo;
-        if(v == 0)
-            vo = 0;
-        else
-            vo = *v;
+    struct InputThreadArgs args = (struct InputThreadArgs){ &win, controller };
+    pthread_t inputThread;
+    pthread_create(&inputThread, NULL, *InputThread, &args);
 
-        printf("%c (%d): %d\n", ko, h, vo);
-    }
-
-    return 0;
-    WindowsInputInit();
-
-    /*struct Dictionary *inputAxes = NewDictionaryAllocate(4);
-    DictAdd(inputAxes, HashInt(LEFT), 'a');
-    DictAdd(inputAxes, HashInt(RIGHT), 'd');
-    DictAdd(inputAxes, HashInt(UP), 'w');
-    DictAdd(inputAxes, HashInt(DOWN), 's');*/
-    return 0;
 
     struct BlockMap *board = NewBlockMap(10, 10);
     struct Buffer *displayBuffer = NewBuffer(20, 10);
@@ -613,11 +735,37 @@ int main()
     {
         DrawBlockMapToBuffer(board, displayBuffer);
         DrawBlockMapToBuffer(a, displayBuffer);
-        struct String *out = DrawBufferToString(displayBuffer);
 
-        printf("\e[1;1H\e[2J");
+        struct String *out = DrawBufferToString(displayBuffer);
         puts(out->chars);
+
+        WindowsWait(&win, 0.1);
+
+
+        pthread_mutex_lock(&controller->mutex);
+
+        if(ControllerAxis(controller, LEFT)->isPressed)
+            a->x--;
+        if(ControllerAxis(controller, RIGHT)->isPressed)
+            a->x++;
+        if(ControllerAxis(controller, UP)->isPressed)
+            a->y--;
+        if(ControllerAxis(controller, DOWN)->isPressed)
+            a->y++;
+
+        if(ControllerAxis(controller, ROTATE_CCW)->isPressed)
+            a->rotation = (a->rotation - 1) % 4;
+        if(ControllerAxis(controller, ROTATE_CW)->isPressed)
+            a->rotation = (a->rotation + 1) % 4;
+
+        printf("%d", a->rotation);
+
+        if(ControllerAxis(controller, ESC)->isPressed)
+            break;
+
+        pthread_mutex_unlock(&controller->mutex);
     }
 
+    printf("done");
     return 0;
 }
